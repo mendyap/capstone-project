9b41a87c2bccfc607b4075909e2905215a5fe1e3
Mendy Apfelbaum <mmapfel@gmail.com>
2021-01-04 20:41:37 -0500

    changed config to .py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+import os
 database_path = os.environ['DATABASE_URL']
 SQLALCHEMY_DATABASE_URI = database_path
 SQLALCHEMY_TRACK_MODIFICATIONS=False
--- models.py
+++ models.py
@@ -4,7 +4,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.config.from_pyfile('config.cfg')
+app.config.from_object('config')
 
 db = SQLAlchemy(app)
 
