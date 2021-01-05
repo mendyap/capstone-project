7bdbd9868520fe7ccab8e58aa6f9b822232ac730
Mendy Apfelbaum <mmapfel@gmail.com>
2021-01-05 18:35:00 -0500

    Added tables and other things

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from models import app, db, Customer
+from datetime import date
 
 @app.route('/<int:id>')
 def index(id):
@@ -11,18 +12,85 @@ def index(id):
         'customer': customer.name
     })
 
-@app.route('/create', methods=['POST'])
-def create_customer():
-    data = request.get_json()
+@app.route('/customers')
+def get_customers():
+    customers = Customer.query.all()
 
-    customer = Customer(name=data['name'])
+    customer_list = []
+
+    for customer in customers:
+        customer_list.append(customer.name)
+
+    return jsonify({
+        'success': True,
+        'customers': customer_list
+    })
+
+
+@app.route('/new_customer', methods=['POST'])
+def create_customer():
+
+    data = request.get_json()
+    today = date.today()
+
+    if not data['name'] or not data['email']:
+        abort(404)
+
+    customer = Customer(name=data['name'],
+                        email=data['email'], join_date=today)
 
     db.session.add(customer)
     db.session.commit()
 
+    new_customer = Customer.query.filter_by(name=data['name']).one_or_none()
+    print('Customer:', new_customer)
+
     return jsonify({
         'success': True,
-        'Customer': customer.name
+        'Customer': new_customer.name
+    })
+
+
+@app.route('/update_customer/<int:id>', methods=['PATCH'])
+def update_customer(id):
+    data = request.get_json()
+
+    customer = Customer.query.filter_by(id=id).one_or_none()
+
+    if not data['name'] and not data['email']:
+        abort(404)
+
+    if data['name']:
+        customer.name = data['name']
+
+    if data['email']:
+        customer.email = data['email']
+
+        db.session.commit()
+
+        updated_customer = Customer.query.filter_by(id=id).one_or_none()
+
+    return jsonify({
+        'success': True,
+        'customer_name': updated_customer.name,
+        'customer_email': updated_customer.email
+    })
+
+
+@app.route('/delete_customer/<int:id>', methods=['DELETE'])
+def delete_customer(id):
+    customer = Customer.query.filter_by(id=id).one_or_none()
+    deleted_name = customer.name
+
+    db.session.delete(customer)
+    db.session.commit()
+
+    customers = customer.query.all()
+
+    return jsonify({
+        'success': True,
+        'deleted_customer': deleted_name,
+        'num_of_remaining_customers': len(customers)
     })
 
 
--- models.py
+++ models.py
@@ -12,4 +12,33 @@ class Customer(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(25))
+    email = db.Column(db.String(50))
+    join_date = db.Column(db.DateTime)
+    orders = db.relationship('Orders', cascade='all, delete', backref='customer')
 
+    def __repr__(self):
+        return '<Customer {}>'.format(self.name)
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=True)
+    brand = db.Column(db.String(30))
+    price = db.Column(db.Integer)
+    available = db.Column(db.Boolean, default=True)
+    orders = db.relationship('Orders', cascade='all, delete', backref='item')
+
+    def __repr__(self):
+        return '<Item: {}, Available: {}>'.format(self.name, self.available)
+
+
+class Orders(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'))
+    item_id = db.Column(db.Integer, db.ForeignKey('item.id'))
+    quantity = db.Column(db.Integer)
+    amount_due = db.Column(db.Integer)
+    amount_paid = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<Customer: {}, Item: {}, Quantity: {}>'.format(self.customer.name, self.item.name, self.quantity)
